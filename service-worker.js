3711ee4051f26f204f1f2993e8f5c1f1ce9cdf8f
veselyroman <veselyroman@seznam.cz>
2026-08-06 12:16:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = "bici-denik-v1";
+const CACHE_NAME = "bici-denik-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./manifest.json",
     "./images/drumset.png",
-    "./images/sticks.png",
-    "./images/snare.png"
+    "./images/icon-180.png",
+    "./images/icon-512.png",
+    "./images/icon-192.png"
 ];
 
 self.addEventListener("install", event => {
@@ -16,10 +17,22 @@ self.addEventListener("install", event => {
     );
 });
 
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
     );
 });
-//v2
+``
